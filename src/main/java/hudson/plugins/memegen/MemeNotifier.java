a3f99acdf7e6ae8611cc54ec11c506219bc800c9
Jon Cairns <jon@joncairns.com>
2012-02-27 17:15:39 +0000

    Removed DescriptorImpl::newInstance for compatability

--- src/main/java/hudson/plugins/memegen/MemeNotifier.java
+++ src/main/java/hudson/plugins/memegen/MemeNotifier.java
@@ -26,38 +26,14 @@ import org.kohsuke.stapler.DataBoundConstructor;
  */
 public class MemeNotifier extends Notifier {
 
-	public boolean enableFailure;
-	public boolean enableSucceed;
-	public boolean enableAlways;
-	private String memeUsername;
-	private String memePassword;
 	private static final Logger LOGGER = Logger.getLogger(MemeNotifier.class.getName());
 	private static final DescriptorImpl DESCRIPTOR = new DescriptorImpl();
 
-	public MemeNotifier() {
-		//System.err.println("MemeNotifier called with NO args");
-		initialize();
-	}
-
 	@DataBoundConstructor
-	public MemeNotifier(boolean enableFailure, boolean enableSucceed, boolean enableAlways) {
+	public MemeNotifier() {
 		//System.err.println("MemeNotifier called with args: "+memeUsername+", "+memePassword+", "+enableFailure+", "+enableSucceed+", "+enableAlways);
-		initialize(enableFailure, enableSucceed, enableAlways);
 	}
 
-	private void initialize() {
-		initialize(
-			DESCRIPTOR.memeEnabledFailure,
-			DESCRIPTOR.memeEnabledSuccess,
-			DESCRIPTOR.memeEnabledAlways);
-	}
-
-	private void initialize(boolean enableFailure, boolean enableSucceed, boolean enableAlways) {
-		this.enableFailure = enableFailure;
-		this.enableSucceed = enableSucceed;
-		this.enableAlways = enableAlways;
-		//System.err.println("Auth: "+memeUsername+", "+memePassword);
-	}
 
 	/*
 	 * (non-Javadoc)
@@ -92,6 +68,10 @@ public class MemeNotifier extends Notifier {
 			}
 		} catch (IOException ie) {
 			LOGGER.log(Level.WARNING, "{0}{1}", new Object[]{"Meme generation failed: ", ie.getMessage()});
+			listener.getLogger().println("Sorry, couldn't create a Meme - check the logs for more detail");
+		} catch (Exception e) {
+			LOGGER.log(Level.WARNING, "{0}{1}", new Object[]{"Meme generation failed: ", e.getMessage()});
+			listener.getLogger().println("Sorry, couldn't create a Meme - check the logs for more detail");
 		}
 	}
 
@@ -105,18 +85,17 @@ public class MemeNotifier extends Notifier {
 	@Override
 	public boolean perform(AbstractBuild<?, ?> build, Launcher launcher,
 		BuildListener listener) throws InterruptedException, IOException {
-		listener.getLogger().println("perform() called");
 
-		if (enableAlways) {
+		if (DESCRIPTOR.memeEnabledAlways) {
 			listener.getLogger().println("Generating Meme...");
 			generate(build, listener);
-		} else if (enableSucceed && build.getResult() == Result.SUCCESS) {
+		} else if (DESCRIPTOR.memeEnabledSuccess && build.getResult() == Result.SUCCESS) {
 			AbstractBuild prevBuild = build.getPreviousBuild();
 			if (prevBuild.getResult() == Result.FAILURE) {
 				listener.getLogger().println("Build has returned to successful, generating Meme...");
 				generate(build, listener);
 			}
-		} else if (enableFailure && build.getResult() == Result.FAILURE) {
+		} else if (DESCRIPTOR.memeEnabledFailure && build.getResult() == Result.FAILURE) {
 			listener.getLogger().println("Build failure, generating Meme...");
 			generate(build, listener);
 		}
@@ -148,17 +127,6 @@ public class MemeNotifier extends Notifier {
 		}
 
 		@Override
-		public Publisher newInstance(StaplerRequest req, JSONObject formData) throws FormException {
-			boolean enableMeme = req.getParameter("enableMeme") != null;
-
-			try {
-				return new MemeNotifier(memeEnabledFailure,memeEnabledSuccess,memeEnabledAlways);
-			} catch (Exception e) {
-				throw new FormException("Failed to initialize campfire notifier - check your campfire notifier configuration settings", e, "");
-			}
-		}
-
-		@Override
 		public boolean configure(StaplerRequest req, JSONObject json) throws FormException {
 			memeUsername = req.getParameter("memeUsername");
 			memePassword = req.getParameter("memePassword");
@@ -168,11 +136,6 @@ public class MemeNotifier extends Notifier {
 			memeEnabledSuccess = req.getParameter("memeEnabledSuccess") != null;
 			memeEnabledFailure = req.getParameter("memeEnabledFailure") != null;
 			System.err.println("Params: "+memeEnabledAlways+", "+memeEnabledSuccess+", "+memeEnabledFailure);
-			try {
-				MemeNotifier memeNotifier = new MemeNotifier(memeEnabledFailure, memeEnabledSuccess, memeEnabledAlways);
-			} catch (Exception e) {
-				throw new FormException("Failed to initialize meme generator - check your global campfire notifier configuration settings", e, "");
-			}
 			save();
 			return super.configure(req, json);
 		}
